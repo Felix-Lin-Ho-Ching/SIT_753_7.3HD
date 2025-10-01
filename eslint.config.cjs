b5f8342f9ed4b0b4268d316414aa8ccc48ad19d5
Ho Ching Lin <felixlinhoching@gmail.com>
2025-10-01 23:56:22 +1000

    ESLint v9 flat config + security remediations (express 4.21.1)

---
+++ eslint.config.cjs
@@ -0,0 +1,10 @@
+﻿const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: { ecmaVersion: "latest", sourceType: "commonjs" },
+    env: { node: true, jest: true },
+    rules: { "no-unused-vars": ["warn", { "args": "none" }] }
+  }
+];
