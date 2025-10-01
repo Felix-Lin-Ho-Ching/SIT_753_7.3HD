c7f0f4a97283a3544286b1eed08d4e39f14a3d91
Ho Ching Lin <felixlinhoching@gmail.com>
2025-10-02 00:43:26 +1000

    fix: ESLint v9 flat config (globals)

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,10 +1,20 @@
 ﻿const js = require('@eslint/js');
+const globals = require('globals');
 
 module.exports = [
   js.configs.recommended,
   {
-    languageOptions: { ecmaVersion: "latest", sourceType: "commonjs" },
-    env: { node: true, jest: true },
-    rules: { "no-unused-vars": ["warn", { "args": "none" }] }
-  }
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { args: 'none' }],
+    },
+  },
 ];
